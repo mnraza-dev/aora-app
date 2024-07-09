306d8527f023ccab109d135780387f8e79ddc60f
mnrazablog <mnraza94@gmail.com>
2024-07-09 19:55:52 +0530

    updated the README.md file

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,7 +1,7 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
-import {images } from "../constants"
-import {icons } from "../constants"
+import {icons } from "../constants";
+
 const FormField = ({
   title,
   value,
